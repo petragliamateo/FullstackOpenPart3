ab57a2886304584ac8bfb046d5e66b07cc23967c
Mateo <petragliamateo@gmail.com>
2022-06-30 11:09:42 -0300

    phoneguide backend 3.3

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (!person) {
+    response.status(404).end();
+    return;
+  }
+  response.json(person);
+})
+
 app.get('/info', (request, response) => {
   response.send(`
   <div>
