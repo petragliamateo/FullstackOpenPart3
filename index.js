af359ceeeb4445587b93b85d0a2dd7b3233ae18b
Mateo <petragliamateo@gmail.com>
2022-06-30 11:32:43 -0300

    phoneguide backend 3.5

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let persons = require('./db.json')
 const app = express()
 const date = new Date;
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
   console.log(persons);
@@ -35,6 +37,20 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((p) => p.id !== id);
   response.status(204).end();
+})
+
+app.post('/api/persons', (request, response) => {
+  console.log(request.body);
+  if (!request.body.name) {
+    return response.status(400).json({error: 'name missing'})
+  }
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+    id: Math.floor(Math.random() * 10000)
+  }
+  persons = persons.concat(person);
+  response.json(persons);
 })
 
 const PORT = 3001
