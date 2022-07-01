53182e9714c909a2c8443ed1ed7dce67a7096e43
Mateo <petragliamateo@gmail.com>
2022-07-01 08:47:21 -0300

    phoneguide backend 3.11

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const date = new Date;
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(morgan((tokens, req, res) => {
   return [
