e820ac50d1a47a41b051b39c08f5e2601ab5a96a
Mateo <petragliamateo@gmail.com>
2022-07-01 18:54:50 -0300

    phoneguide backend 3.15

--- index.js
+++ index.js
@@ -47,12 +47,12 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  /*
-  const id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204).end();
-  */
-  console.log('delete......');
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      console.log(result);
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
